dd95c9af696791b9855142349a03cca33b8c5662
youngho1430 <81900475+youngho1430@users.noreply.github.com>
2022-02-03 20:40:36 +0900

    처음올리는거

---
+++ hijava/src/hijava/Helloclass.java
@@ -0,0 +1,10 @@
+package hijava;
+
+public class Helloclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello gitgitgitigtigtitgigtigtitgitgitgigti");
+	}
+
+}
