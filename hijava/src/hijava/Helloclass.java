1b240b347c56a98a650fca0ba19ebc826f7d119b
youngho1430 <81900475+youngho1430@users.noreply.github.com>
2022-02-03 20:43:34 +0900

    깃 추가했오

--- hijava/src/hijava/Helloclass.java
+++ hijava/src/hijava/Helloclass.java
@@ -5,6 +5,7 @@ public class Helloclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello gitgitgitigtigtitgigtigtitgitgitgigti");
+		System.out.println("hello 추가했오 깃");
 	}
 
 }
